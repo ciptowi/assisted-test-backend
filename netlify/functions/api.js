2f18822447f3562351a5449210083bf0bde7f9b0
Cipto <ciptowidarto@gmail.com>
2022-11-13 22:29:09 +0700

    fix: setup netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const router = require('../src/routers');
+const router = require('../../src/routers');
 const serverless = require('serverless-http');
 
 var corsOptions = {
